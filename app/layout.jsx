9e02adb7cc20b36f3d00cf2097f964a9ce0e4c6c
Hassam Tariq <hassamtariq399@gmail.comm>
2026-05-09 13:51:26 +0500

    font add

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,19 @@
 // REMOVE all localFont imports and config
+import localFont from 'next/font/local'
 import Header from './Component/header'
 import TopBar from './Component/topbar'
 import Footer from './Component/footer'
 import './globals.css'
-
+const formaDJR =localFont({
+  src:[{
+    path:"./assets/fonts/FormaDJRText-Medium-Testing.otf"
+  }],
+  variable:"--font-forma",
+  display:"swap"
+})
 export default function RootLayout({ children }) {
   return (
-    <html> 
+    <html className={formaDJR.variable}> 
       <body>
         <TopBar/>
         <Header/>
